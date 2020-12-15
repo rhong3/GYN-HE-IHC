b58b0b20795dedbeec29ea6b373942efce314b04
rhong3 <michaelhong1995@gmail.com>
2020-12-15 11:49:37 -0500

    12.15.2

--- scripts/Alignment.py
+++ scripts/Alignment.py
@@ -4,35 +4,33 @@ import cv2
 import numpy as np
 from PIL import Image
 
-# slide = OpenSlide('../align/collection_0000063578_2020-10-13 22_19_26.scn')
-# print(slide.level_dimensions)
-# upperleft = [int(slide.properties['openslide.bounds-x']),
-#              int(slide.properties['openslide.bounds-y'])]
-# lowerright = [int(int(slide.properties['openslide.bounds-width'])/4),
-#               int(int(slide.properties['openslide.bounds-height'])/4)]
-# x = int(slide.properties['openslide.bounds-width'])-int(slide.properties['openslide.bounds-x'])
-# y = int(slide.properties['openslide.bounds-height'])-int(slide.properties['openslide.bounds-y'])
-#
-# print([x, y])
-#
-# tnl = slide.read_region(upperleft, 1, lowerright).convert('RGB')
-# tnl.save('../align/ori.jpg')
-#
-# ihc = OpenSlide('../align/collection_0000063573_2020-10-14 14_11_00.scn')
-# print(ihc.level_dimensions)
-# upperleft = [int(ihc.properties['openslide.bounds-x']),
-#              int(ihc.properties['openslide.bounds-y'])]
-# lowerright = [int(int(ihc.properties['openslide.bounds-width'])/4),
-#               int(int(ihc.properties['openslide.bounds-height'])/4)]
-# x = int(ihc.properties['openslide.bounds-width'])-int(ihc.properties['openslide.bounds-x'])
-# y = int(ihc.properties['openslide.bounds-height'])-int(ihc.properties['openslide.bounds-y'])
-#
-# print([x, y])
-#
-# itnl = ihc.read_region(upperleft, 1, lowerright).convert('RGB')
-# itnl.save('../align/ihc.jpg')
+slide = OpenSlide('../align/collection_0000063578_2020-10-13 22_19_26.scn')
+print(slide.level_dimensions)
+upperleft = [int(slide.properties['openslide.bounds-x']),
+             int(slide.properties['openslide.bounds-y'])]
+lowerright = [int(int(slide.properties['openslide.bounds-width'])/4),
+              int(int(slide.properties['openslide.bounds-height'])/4)]
+x = int(slide.properties['openslide.bounds-width'])-int(slide.properties['openslide.bounds-x'])
+y = int(slide.properties['openslide.bounds-height'])-int(slide.properties['openslide.bounds-y'])
 
+print([x, y])
 
+tnl = slide.read_region(upperleft, 1, lowerright).convert('LA')
+tnl.save('../align/ori.png')
+
+ihc = OpenSlide('../align/collection_0000063573_2020-10-14 14_11_00.scn')
+print(ihc.level_dimensions)
+upperleft = [int(ihc.properties['openslide.bounds-x']),
+             int(ihc.properties['openslide.bounds-y'])]
+lowerright = [int(int(ihc.properties['openslide.bounds-width'])/4),
+              int(int(ihc.properties['openslide.bounds-height'])/4)]
+x = int(ihc.properties['openslide.bounds-width'])-int(ihc.properties['openslide.bounds-x'])
+y = int(ihc.properties['openslide.bounds-height'])-int(ihc.properties['openslide.bounds-y'])
+
+print([x, y])
+
+itnl = ihc.read_region(upperleft, 1, lowerright).convert('LA')
+itnl.save('../align/ihc.png')
 
 MAX_FEATURES = 500
 GOOD_MATCH_PERCENT = 0.15
@@ -61,7 +59,7 @@ def alignImages(im1, im2):
 
     # Draw top matches
     imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
-    cv2.imwrite("matches.jpg", imMatches)
+    cv2.imwrite("matches.png", imMatches)
 
     # Extract location of good matches
     points1 = np.zeros((len(matches), 2), dtype=np.float32)
@@ -79,4 +77,28 @@ def alignImages(im1, im2):
     im1Reg = cv2.warpPerspective(im1, h, (width, height))
 
     return im1Reg, h
+
+
+# Read reference image
+refFilename = "../align/ori.png"
+print("Reading reference image : ", refFilename)
+imReference = cv2.imread(refFilename, cv2.IMREAD_COLOR)
+
+# Read image to be aligned
+imFilename = "../align/ihc.png"
+print("Reading image to align : ", imFilename)
+im = cv2.imread(imFilename, cv2.IMREAD_COLOR)
+
+print("Aligning images ...")
+# Registered image will be resotred in imReg.
+# The estimated homography will be stored in h.
+imReg, h = alignImages(im, imReference)
+
+# Write aligned image to disk.
+outFilename = "../align/aligned.png"
+print("Saving aligned image : ", outFilename);
+cv2.imwrite(outFilename, imReg)
+
+# Print estimated homography
+print("Estimated homography : \n", h)
 
