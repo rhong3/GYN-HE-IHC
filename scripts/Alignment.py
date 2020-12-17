c6540e4e09aa88a5e3c400f0966c851c984f18e9
rhong3 <michaelhong1995@gmail.com>
2020-12-17 14:27:22 -0500

    12.17.7  de facto

--- scripts/Alignment.py
+++ scripts/Alignment.py
@@ -1,28 +1,27 @@
 # IHC and HE alignment
 from openslide import OpenSlide
-import cv2
 import numpy as np
 from PIL import Image
-import staintools
 import skimage.morphology as skm
 from scipy.ndimage.interpolation import rotate
 
-std = staintools.read_image("../colorstandard.png")
-std = staintools.LuminosityStandardizer.standardize(std)
+
+def read_valid(pathtosld, tp):
+    slide = OpenSlide(pathtosld)
+    upperleft = [int(slide.properties['openslide.bounds-x']),
+                 int(slide.properties['openslide.bounds-y'])]
+    lowerright = [int(int(slide.properties['openslide.bounds-width']) / 16),
+                  int(int(slide.properties['openslide.bounds-height']) / 16)]
+    x = int(slide.properties['openslide.bounds-width']) - int(slide.properties['openslide.bounds-x'])
+    y = int(slide.properties['openslide.bounds-height']) - int(slide.properties['openslide.bounds-y'])
+    print("{} valid size: ".format(tp))
+    print([x, y])
+    outimg = slide.read_region(upperleft, 2, lowerright).convert('RGB')
+
+    return outimg
 
 
-# Tile color normalization
-def normalization(img, sttd):
-    img = np.array(img)[:, :, :3]
-    img = staintools.LuminosityStandardizer.standardize(img)
-    normalizer = staintools.StainNormalizer(method='vahadane')
-    normalizer.fit(sttd)
-    img = normalizer.transform(img)
-    img = Image.fromarray(img.astype('uint8'), 'RGB')
-    return img
-
-
-def binarize(img, name):
+def binarize(img):
     img = np.array(img)[:, :, :3]
     img = np.nan_to_num(img, nan=0, posinf=0, neginf=0)
     maska = (img[:, :, :3] > 220).astype(np.uint8)
@@ -40,12 +39,8 @@ def binarize(img, name):
     mask = skm.binary_closing(mask)
     mask = skm.binary_dilation(mask)
     mask = skm.binary_erosion(mask)
-    mask = skm.remove_small_objects(mask, min_size=100000, connectivity=1, in_place=False)
-    mask = skm.remove_small_holes(mask, area_threshold=100000, connectivity=1, in_place=False)
-
-    mask_out = mask*255
-    mask_out = Image.fromarray(mask_out.astype('uint8'), 'RGB')
-    mask_out.save(name)
+    mask = skm.remove_small_objects(mask, min_size=400000, connectivity=1, in_place=False)
+    mask = skm.remove_small_holes(mask, area_threshold=200000, connectivity=1, in_place=False)
 
     return mask
 
@@ -56,42 +51,29 @@ def pad_with(vector, pad_width, iaxis, kwargs):
     vector[-pad_width[1]:] = pad_value
 
 
-def optimize(imga, imgb):
+def optimize(imga, imgb, angle=30):
+    rot = int(360/angle)
     imgb = imgb[:, :, 0]
     pdd = int(np.sqrt(imgb.shape[0]**2+imgb.shape[1]**2)-np.amin(imga.shape[:2])+1)
     imga = imga[:, :, 0]
     imga = np.pad(imga, pdd, mode=pad_with).astype('uint8')
 
-    ori_pad_out = imga*255
-    ori_pad_out_RGB = np.empty([ori_pad_out.shape[0], ori_pad_out.shape[1], 3])
-    ori_pad_out_RGB[:, :, 0] = ori_pad_out
-    ori_pad_out_RGB[:, :, 1] = ori_pad_out
-    ori_pad_out_RGB[:, :, 2] = ori_pad_out
-    ori_pad_out_RGB = Image.fromarray(ori_pad_out_RGB.astype('uint8'), 'RGB')
-    ori_pad_out_RGB.save('../align/ori_pad.jpg')
-
     ori_canvas = np.zeros(imga.shape)
     ori_canvas[pdd:pdd+imgb.shape[0], pdd:pdd+imgb.shape[1]] = imgb
 
-    ori_canvas_out = ori_canvas*255
-    ori_canvas_out_RGB = np.empty([ori_canvas_out.shape[0], ori_canvas_out.shape[1], 3])
-    ori_canvas_out_RGB[:, :, 0] = ori_canvas_out
-    ori_canvas_out_RGB[:, :, 1] = ori_canvas_out
-    ori_canvas_out_RGB[:, :, 2] = ori_canvas_out
-    ori_canvas_out_RGB = Image.fromarray(ori_canvas_out_RGB.astype('uint8'), 'RGB')
-    ori_canvas_out_RGB.save('../align/ori_canvas.jpg')
-
     globalmax = 0
+    globalbest_canvas = ori_canvas
+    globalmax_coor = [0, 0, pdd, pdd, pdd]
     for t in range(2):
         if t == 0:
             imgbt = imgb
         else:
             imgbt = np.transpose(imgb)
-        for r in range(36):
-            imgx = rotate(imgbt, r*10)
+        for r in range(rot):
+            imgx = rotate(imgbt, r*angle)
             imgx = (imgx > 0.5).astype(np.uint8)
-            imgx = skm.remove_small_objects(imgx, min_size=1000, connectivity=1, in_place=False)
-            imgx = skm.remove_small_holes(imgx, area_threshold=1000, connectivity=1, in_place=False)
+            imgx = skm.remove_small_objects(imgx, min_size=100, connectivity=1, in_place=False)
+            imgx = skm.remove_small_holes(imgx, area_threshold=100, connectivity=1, in_place=False)
             istart = 0
             jstart = 0
             iend = imga.shape[0]-imgx.shape[0]
@@ -116,9 +98,6 @@ def optimize(imga, imgb):
                             best_coor = [t, r, i, j, pdd]
                             best_canvas = canvas
                             newmax = True
-                            print("new local max: {}".format(maxx))
-                            print('new local best coordinate: ')
-                            print(best_coor)
                 istart = np.amin([best_coor[2], sndbest_coor[2]])
                 iend = np.amax([best_coor[2], sndbest_coor[2]])
                 jstart = np.amin([best_coor[3], sndbest_coor[3]])
@@ -135,52 +114,47 @@ def optimize(imga, imgb):
                 globalmax = maxx
                 globalmax_coor = best_coor
                 globalbest_canvas = best_canvas
-                print("new global max: {}".format(globalmax))
+                print("new global max overlap: {}".format(globalmax))
                 print('new global best coordinate: ')
                 print(globalmax_coor)
 
-    print("Global max: {}".format(globalmax))
+    print("Global max overlap: {}".format(globalmax))
     print('Global best coordinate: ')
     print(globalmax_coor)
 
-    best_canvas_out = globalbest_canvas*255
-    best_canvas_out_RGB = np.empty([best_canvas_out.shape[0], best_canvas_out.shape[1], 3])
-    best_canvas_out_RGB[:, :, 0] = best_canvas_out
-    best_canvas_out_RGB[:, :, 1] = best_canvas_out
-    best_canvas_out_RGB[:, :, 2] = best_canvas_out
-    best_canvas_out_RGB = Image.fromarray(best_canvas_out_RGB.astype('uint8'), 'RGB')
-    best_canvas_out_RGB.save('../align/best_canvas.jpg')
+    return globalmax_coor, globalmax, globalbest_canvas, imga
 
 
-slide = OpenSlide('../align/collection_0000063578_2020-10-13 22_19_26.scn')
-print(slide.level_dimensions)
-upperleft = [int(slide.properties['openslide.bounds-x']),
-             int(slide.properties['openslide.bounds-y'])]
-lowerright = [int(int(slide.properties['openslide.bounds-width'])/16),
-              int(int(slide.properties['openslide.bounds-height'])/16)]
-x = int(slide.properties['openslide.bounds-width'])-int(slide.properties['openslide.bounds-x'])
-y = int(slide.properties['openslide.bounds-height'])-int(slide.properties['openslide.bounds-y'])
+def cvs_to_img(cvss):
+    canvas_out = cvss * 255
+    if len(canvas_out.shape) == 3:
+        canvas_out_RGB = Image.fromarray(canvas_out.astype('uint8'), 'RGB')
+    else:
+        canvas_out_RGB = np.empty([canvas_out.shape[0], canvas_out.shape[1], 3])
+        canvas_out_RGB[:, :, 0] = canvas_out
+        canvas_out_RGB[:, :, 1] = canvas_out
+        canvas_out_RGB[:, :, 2] = canvas_out
+        canvas_out_RGB = Image.fromarray(canvas_out_RGB.astype('uint8'), 'RGB')
 
-print([x, y])
-
-tnl = slide.read_region(upperleft, 2, lowerright).convert('RGB')
-tnl = binarize(tnl, '../align/ori.jpg')
-
-
-ihc = OpenSlide('../align/collection_0000063573_2020-10-14 14_11_00.scn')
-print(ihc.level_dimensions)
-upperleft = [int(ihc.properties['openslide.bounds-x']),
-             int(ihc.properties['openslide.bounds-y'])]
-lowerright = [int(int(ihc.properties['openslide.bounds-width'])/16),
-              int(int(ihc.properties['openslide.bounds-height'])/16)]
-x = int(ihc.properties['openslide.bounds-width'])-int(ihc.properties['openslide.bounds-x'])
-y = int(ihc.properties['openslide.bounds-height'])-int(ihc.properties['openslide.bounds-y'])
-
-print([x, y])
-
-itnl = ihc.read_region(upperleft, 2, lowerright).convert('RGB')
-itnl = binarize(itnl, '../align/ihc.jpg')
-
-optimize(tnl, itnl)
+    return canvas_out_RGB
 
 
+def overlap(cvsa, cvsb, coordinate):
+    cvsc = (cvsa+cvsb)/2
+    cvsc = cvsc[coordinate[4]:int(cvsc.shape[0]-coordinate[4]), coordinate[4]:int(cvsc.shape[1]-coordinate[4])]
+
+    return cvsc
+
+
+if __name__ == '__main__':
+    tnl = read_valid('../align/collection_0000063578_2020-10-13 22_19_26.scn', 'H&E')
+    tnl = binarize(tnl)
+
+    itnl = read_valid('../align/collection_0000063576_2020-10-14 14_13_37.scn', 'IHC')
+    itnl = binarize(itnl)
+
+    coor, gmax, cvs, he_cvs = optimize(tnl, itnl, 90)
+
+    ovl = overlap(cvs, he_cvs, coor)
+    ovl = cvs_to_img(ovl)
+    ovl.save('../align/overlap.jpg')
