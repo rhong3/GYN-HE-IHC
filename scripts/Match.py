15a446283528faacaf7c2fde8dc8f4674545b876
rhong3 <michaelhong1995@gmail.com>
2021-01-08 11:28:12 -0500

    1.8.1

---
+++ scripts/Match.py
@@ -0,0 +1,14 @@
+# Match IHC-labeled tiles
+import pandas as pd
+
+if __name__ == '__main__':
+    ref = pd.read_csv('../align/final_summary.csv', header=0)
+    for idx, row in ref.iterrows():
+        IHC = row['IHC_ID'].split('-')[-1]
+        for level in range(1, 4):
+            tile_dict = pd.read_csv('../tiles/{}/level{}/{}_dict.csv'.format(row['Patient_ID'], level, row['H&E_ID']),
+                                    header=0)
+            label_dict = pd.read_csv('../autolabel/{}/{}/{}/ratio_level{}.csv'.format(row['Patient_ID'], row['H&E_ID'],
+                                                                                      row['IHC_ID'], level), header=0)
+
+
